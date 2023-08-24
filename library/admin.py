6721a893a49a46dbbc856c36b7c74a07159e38b4
Muhammed Enes Ekinci <ekincimuhammedenes13@gmail.com>
2023-08-24 22:41:58 +0300

    admin-model updated

--- library/admin.py
+++ library/admin.py
@@ -7,17 +7,18 @@ from .models import Book
 @admin.register(Writer)
 class WriterAdmin(admin.ModelAdmin):
 
+    prepopulated_fields = {"slug_writer_name" : ('writer_name',)}
     list_display = ["writer_name", "writer_category"]
-    list_display_links = ["writer_name", "writer_category"]
+    list_display_links = ["writer_name"]
     list_filter = ["writer_name"]
     search_fields = ["writer_name"]
-
+    list_editable = ['writer_category']
+     
     class Meta:
         model = Writer 
 
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
 
+class BookAdmin(admin.ModelAdmin):
     list_display = ["writer_name", "book_name"]
     list_display_links = ["writer_name", "book_name"]
     list_filter = ["writer_name", "book_name"]
@@ -25,4 +26,5 @@ class BookAdmin(admin.ModelAdmin):
 
     class Meta:
         model = Book
-
+    
+admin.site.register(Book, BookAdmin)
--- library/models.py
+++ library/models.py
@@ -15,6 +15,7 @@ class Writer(models.Model):
         ('Korku-Gerilim', 'thriller')
     )
     writer_name = models.CharField(max_length=20, verbose_name="Writer Name")
+    slug_writer_name = models.SlugField(max_length=20, default='')
     writer_category = models.CharField(max_length=50,choices=categories, verbose_name="Writer Category")
     writer_image = models.ImageField(
         upload_to="static/images/writers/", verbose_name="Writer's Image"
@@ -24,6 +25,7 @@ class Writer(models.Model):
         verbose_name="Style of Write's Image",
     )
     writer_bio = models.TextField(verbose_name="Writer's Bio")
+    
 
     def __str__(self):
         return self.writer_name
@@ -32,12 +34,6 @@ class Book(models.Model):
     
     writer_name = models.ForeignKey(Writer, on_delete=models.CASCADE)
     book_name = models.CharField(max_length=20, verbose_name="Book Name")
-    # book_image = models.ImageField(
-    #     upload_to="static/images/books/", verbose_name="Book Image"
-    # )
-    # book_image_style = models.TextField(
-    #     default="center", verbose_name="Book Image Style"
-    # )
     book_context = RichTextField(verbose_name="Book Context")
     book_concise_swords = RichTextField(verbose_name="Book Concise Words")
     book_kind = models.CharField(max_length=30, verbose_name='Book Kind')
